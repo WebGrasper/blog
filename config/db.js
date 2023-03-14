9ce337a3a6d4282c620919ee9e6085b6f6770af0
Mohammad Amaan <86742621+mohdamaan570@users.noreply.github.com>
2023-03-14 16:11:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,9 +5,8 @@ const db = ()=>{
     /*mongoose.set is used to enable the strict mode.
     it means the db will only save the data of attributes those are define in schema*/
     mongoose.set('strictQuery', true);
-
     //database connection
-    mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ac-7ierchj-shard-00-00.aekwpjg.mongodb.net:27017,ac-7ierchj-shard-00-01.aekwpjg.mongodb.net:27017,ac-7ierchj-shard-00-02.aekwpjg.mongodb.net:27017/?ssl=true&replicaSet=atlas-12t5gz-shard-0&authSource=admin&retryWrites=true&w=majority`,).then(()=>{
+    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@blog.aekwpjg.mongodb.net/?retryWrites=true&w=majority`,).then(()=>{
         console.log("Database connected successfully!");
     }).catch((err)=>{
         console.log(err);
